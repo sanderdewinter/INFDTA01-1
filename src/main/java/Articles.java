9e98b699d429d4fb3603b208f5f83d5498bcfad7
Davey <davey.witter@live.nl>
2016-02-09 10:46:09 +0100

    Articles

---
+++ src/main/java/Articles.java
@@ -0,0 +1,15 @@
+public class Articles {
+    private long id;
+
+    public Articles(long id) {
+        this.id = id;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
